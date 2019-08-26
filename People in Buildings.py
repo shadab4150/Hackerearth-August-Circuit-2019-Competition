39cbc809877110f515237dc2e4ebc35fea5b0609
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-08-27 00:19:46 +0530

    Update People in Buildings.py

--- People in Buildings.py
+++ People in Buildings.py
@@ -1,3 +1,14 @@
+"""
+https://www.hackerearth.com/challenges/competitive/august-circuits-19/approximate/save-hawkins-3d060dfa/
+"""
+"""
+There N people in your city. An id is associated with each of the citizens and numbered from 1 to N. 
+You know the exact coordinates (x, y) of the location where each citizen lives. Your task is to move all these people to a building. 
+You can build a building at any time. The building will be sustainable only if at least G people are moved to the building. 
+You are required to move each of the citizens by using the minimum possible time. People can move one unit distance per unit time and the distance of two locations in the 2D-plane is equal to their Euclidean distance. 
+Your task is to determine a solution so that the required time to move all the citizens to the building is minimized.
+"""
+
 import math
 n=int(input())
 def distance(k,l,m):

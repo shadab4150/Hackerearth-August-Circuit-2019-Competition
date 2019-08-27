f8a4c3aae84f3788eda5a73777e89e1761ab559e
shadab4150 <37144992+shadab4150@users.noreply.github.com>
2019-08-27 16:46:22 +0530

    Update Health of person.py

--- Health of person.py
+++ Health of person.py
@@ -1,3 +1,8 @@
+"""
+https://www.hackerearth.com/challenges/competitive/august-circuits-19/algorithm/attack-of-the-mind-flayer-3-119b5d47/
+"""
+import gc
+
 for i in range(int(input())):
     n,m=[int(x) for x in input().split()]
     hp=list(map(int,input().split()))
@@ -5,29 +10,34 @@ for i in range(int(input())):
     
     red=list(map(int,input().split()))
     arr=[]
-    for i in range(1,m+1):
-        for j in range(i-1,n,i):
-            hp[j]=hp[j]-red[i-1]
-        for k in range(i-1,len(hp)):
+    
+    for i in range(m):
+        for j in range(i,n,i+1):
+    
+            hp[j]=hp[j]-red[i]
+    
+        for k in range(i,n):
             if hp[k]<=0:
-                arr.append([i,k+1])
-        for i in range(len(hp)):
+                arr.append([i+1,k+1])
+        for i in range(n):
             if hp[i]<=0:
                 hap[i]=0
         hp=hap.copy()
     
-    for i in hp:
-        if i>0:
-            und=hp.index(i)+1
-    for rt in range(1,n+1):
-        if rt==und:
+    
+    
+    for i in range(1,n+1):
+        if hp[i-1]>0:
             print(-1)
             
-        for i in arr:
-            if i[1]==rt and i[1]!=und:
-                print(i[0])
+        for s in arr:
+            if s[1]==i:
+                print(s[0])
                 break
-                
+    gc.collect()   
+    
+        
+            
             
             
         
